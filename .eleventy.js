09c11bd7fcfabe63c899a57512424a5e27f34fc8
Bára Trýznová <majinkam23@gmail.com>
2022-12-09 14:29:08 +0100

    galerie_finished

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
 	// Zkopírovat images/ do _site/images
 	eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("icons");
+  eleventyConfig.addPassthroughCopy("js");
 
 	// Zkopírovat css/ to _site/css/
 	eleventyConfig.addPassthroughCopy("css");
